d8743e0ca47c93d336c46a640ae2321b6691ec75
a kl <anastasiya.klyatysheva@mail.ru>
2020-09-26 20:51:42 +0300

    hello

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $('.block-item').onclick(() => {
+        document.getElementsByClassName("block-item-form").style.backgroundImage = "url('../images/Pink.png')";
+    })
+})
+
