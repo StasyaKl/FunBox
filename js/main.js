2bd8f2be9031fa47d82b8580c72927a48dac690a
a kl <anastasiya.klyatysheva@mail.ru>
2020-09-28 22:58:12 +0300

    'finish'

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 $(document).ready(() => {
-    $('.block-item').onclick(() => {
-        document.getElementsByClassName("block-item-form").style.backgroundImage = "url('../images/Pink.png')";
-    })
-})
+    $(".block-item").on("click", function () {
+        if(!this.classList.contains('disabled')) {
+            $(this).children('.block-item-form').toggleClass('active');
+            $(this).children('.block-item-form').children('.block-item-card').children('.weight').toggleClass('active');
+            $(this).children('.block-item-block').hide();
+            $(this).children('.block-item-select').show();
+            $(this).children('.block-item-form').children('.block-item-card').children('.block-item-text').hide();
+            $(this).children('.block-item-form').children('.block-item-card').children('.block-item-text-select').show();
+        }
+        if(!$(this).children('.block-item-form').hasClass('active')) {
+            $(this).children('.block-item-block').show();
+            $(this).children('.block-item-select').hide();
+            $(this).children('.block-item-form').children('.block-item-card').children('.block-item-text').show();
+            $(this).children('.block-item-form').children('.block-item-card').children('.block-item-text-select').hide();
+        }
+    });
 
+    function disabled(x) {
+        x = $(".disabled");
+        x.children('.block-item-block').hide();
+        x.children('.block-item-select').hide();
+        x.children('.block-item-disabled').show();
+        x.children('.block-item-form').addClass('disabled');
+        x.children('.block-item-form').children('.block-item-card').children('.weight').toggleClass('disabled');
+        x.children('.block-item-form').children('.block-item-card').children('.block-item-text').css('color', '#b3b3b3');
+        x.children('.block-item-form').children('.block-item-card').children('.title').css('color', '#b3b3b3');
+        x.children('.block-item-form').children('.block-item-card').children('.features').css('color', '#b3b3b3');
+    }
+    disabled();
+})
